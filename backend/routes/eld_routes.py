a16b46c03198c017403a9557e4640fc7db098c9d
DulankaEdiriweera <kawminiediriweera21@gmail.com>
2026-03-03 11:42:24 +0530

    Add Progress component

--- backend/routes/eld_routes.py
+++ backend/routes/eld_routes.py
@@ -106,37 +106,3 @@ def predict_eld():
     })
 
 
-# -------------------------------
-# Progress Checking Route
-# -------------------------------
-#progress checking
-@eld_bp.route("/latest_progress", methods=["GET"])
-def get_latest_progress():
-
-    assessments = list(
-        mongo.db.eld_assessments
-        .find()
-        .sort("created_at", 1)
-    )
-
-    if len(assessments) < 2:
-        return jsonify({
-            "message": "Not enough assessments to calculate progress"
-        })
-
-    previous = assessments[-2]
-    latest = assessments[-1]
-
-    percentage_change = round(
-        latest["overall_percentage"] - previous["overall_percentage"], 2
-    )
-
-    return jsonify({
-        "previous_percentage": previous["overall_percentage"],
-        "latest_percentage": latest["overall_percentage"],
-        "percentage_change": percentage_change,
-        "previous_level": previous["eld_level"],
-        "latest_level": latest["eld_level"],
-        "previous_date": previous["created_at"],
-        "latest_date": latest["created_at"]
-    })
